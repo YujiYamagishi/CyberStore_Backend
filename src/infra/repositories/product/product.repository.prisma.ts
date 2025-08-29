07788bf6e6ead72911c1c66d0106898cd1b56778
Fernanda Romanoel <fernanda.romanoel@estudante.iftm.edu.br>
2025-08-29 16:55:15 -0300

    feat/9: add GET endpoint api/product/{id}

--- src/infra/repositories/product/product.repository.prisma.ts
+++ src/infra/repositories/product/product.repository.prisma.ts
@@ -1,12 +1,13 @@
 import { PrismaClient } from "@prisma/client";
 import { Product } from "../../../domain/product/entity/product";
 import { ProductGateway } from "../../../domain/product/gateway/product.gateway";
+import { SmartphoneSpecDto } from "../../../usecases/product/listById.usecase";
 
 export class ProductRepositoryPrisma implements ProductGateway {
 
-    private constructor(private readonly prismaClient: PrismaClient){} 
+    private constructor(private readonly prismaClient: PrismaClient) { }
 
-    public static create(prismaClient: PrismaClient){
+    public static create(prismaClient: PrismaClient) {
         return new ProductRepositoryPrisma(prismaClient);
     }
 
@@ -15,6 +16,50 @@ export class ProductRepositoryPrisma implements ProductGateway {
         return [];
     }
 
+    public async listById(id: number): Promise<{ product: Product | undefined; smartphoneSpec?: SmartphoneSpecDto | null; }> {
+        const p = await this.prismaClient.product.findUnique({
+            where: { id },
+            include: { smartphoneSpec: true }
+        });
+
+        if (!p) {
+            return { product: undefined, smartphoneSpec: null };
+        }
+
+        const product = Product.with({
+            id: p.id,
+            name: p.name,
+            description: p.description,
+            brand: p.brand,
+            price: Number(p.price),
+            discounted_price: Number(p.discounted_price),
+            stock: p.stock,
+            url_image: p.url_image,
+            created_at: p.created_at,
+            updated_at: p.update_at,
+            id_category: p.id_category,
+            tag: p.tag,
+            id_specs_smartphone: p.id_specs_smartphone
+        });
+
+        let smartphoneSpec: SmartphoneSpecDto | null = null;
+        if (p.smartphoneSpec) {
+            smartphoneSpec = {
+                screen_size: p.smartphoneSpec.screen_size,
+                cpu: p.smartphoneSpec.cpu,
+                total_cores: p.smartphoneSpec.total_cores,
+                main_camera: p.smartphoneSpec.main_camera,
+                front_camera: p.smartphoneSpec.front_camera,
+                battery: p.smartphoneSpec.battery,
+                created_at: p.smartphoneSpec.created_at,
+                updated_at: p.smartphoneSpec.update_at
+            };
+        }
+
+        return { product, smartphoneSpec };
+    }
+
+    
     public async listByTag(tag: string): Promise<Product[]> {
         const products = await this.prismaClient.product.findMany({
             where: {
---
+++ src/usecases/product/listById.usecase.ts
@@ -0,0 +1,81 @@
+import { Product } from "../../domain/product/entity/product";
+import { ProductGateway } from "../../domain/product/gateway/product.gateway";
+import { Usecase } from "../usecase";
+
+export type ListProductByIdInputDto = {
+    id: number;
+};
+
+export type SmartphoneSpecDto = {
+    screen_size: string;
+    cpu: string;
+    total_cores: string;
+    main_camera: string;
+    front_camera: string;
+    battery: string;
+    created_at: Date;
+    updated_at?: Date | undefined | null;
+};
+
+export type ListProductByIdOutputDto = {
+    products: {
+        id: number;
+        name: string;
+        description: string;
+        brand: string;
+        price: number;
+        discounted_price: number;
+        stock: number;
+        url_image: string;
+        created_at: Date;
+        updated_at?: Date | undefined;
+        id_category: number;
+        tag: string;
+        id_specs_smartphone?: number | null ;
+        smartphoneSpec?: SmartphoneSpecDto | null;
+    }
+};
+
+export class ListProductByIdUseCase implements Usecase<ListProductByIdInputDto, ListProductByIdOutputDto> {
+    private constructor(private readonly productGateway: ProductGateway) { }
+
+    public static create(productGateway: ProductGateway) {
+        return new ListProductByIdUseCase(productGateway);
+    }
+
+    public async execute(input: ListProductByIdInputDto): Promise<ListProductByIdOutputDto> {
+        const { product, smartphoneSpec } = await this.productGateway.listById(input.id);
+
+        if (!product) {
+            throw new Error("Product not found");
+        }
+        return this.presentOutput(product, smartphoneSpec);
+    }
+
+    private presentOutput(product: Product, smartphoneSpec?: SmartphoneSpecDto | null): ListProductByIdOutputDto {
+        const base = {
+            id: product.id!,
+            name: product.name,
+            description: product.description,
+            brand: product.brand,
+            price: product.price,
+            discounted_price: product.discounted_price,
+            stock: product.stock,
+            url_image: product.url_image,
+            created_at: product.created_at,
+            updated_at: product.updated_at,
+            id_category: product.id_category,
+            tag: product.tag,
+            id_specs_smartphone: product.id_specs_smartphone ?? null,
+        };
+
+        return {
+            products: {
+                ...base,
+                ...(product.id_specs_smartphone != null && smartphoneSpec
+                    ? { smartphoneSpec }
+                    : {})
+            }
+        };
+    }
+}
