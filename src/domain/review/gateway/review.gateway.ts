e9de34b0e38fd8e8336fd3dad53381beba942b80
fernando7492 <emidio8000@gmail.com>
2025-09-01 15:28:46 -0300

    feat/10: add route /api/reviews/comment/{product_id}

--- src/domain/review/gateway/review.gateway.ts
+++ src/domain/review/gateway/review.gateway.ts
@@ -1,6 +1,8 @@
 import { Review } from "../entity/review";
+import { PaginatedReviews } from "../entity/review-paginated";
 
 export interface ReviewGateway{
     list(): Promise<Review[]>;
+    listByProductId(productId: number, page: number, pageSize:number):Promise<PaginatedReviews>;
 
 }
---
+++ src/infra/repositories/reviews/review.repository.prisma.ts
@@ -0,0 +1,59 @@
+import { PrismaClient } from "@prisma/client";
+import { PaginatedReviews, ReviewData } from "../../../domain/review/entity/review-paginated";
+import { ReviewGateway } from "../../../domain/review/gateway/review.gateway";
+import { Review } from "../../../domain/review/entity/review";
+
+export class ReviewRepositoryPrisma implements ReviewGateway {
+    private constructor(private readonly prismaClient: PrismaClient) { }
+
+    public static create(prismaClient: PrismaClient) {
+        return new ReviewRepositoryPrisma(prismaClient);
+    }
+
+    public async list(): Promise<Review[]> {
+        return [];
+    }
+    
+    public async listByProductId(productId: number, page: number, pageSize: number): Promise<PaginatedReviews> {
+        const skip = (page - 1) * pageSize;
+        const take = pageSize;
+
+        const totalReviews = await this.prismaClient.review.count({
+            where: {
+                id_product: productId,
+            }
+        });
+
+        const reviewsFromDb = await this.prismaClient.review.findMany({
+            where: {
+                id_product: productId,
+            },
+            skip: skip,
+            take: take,
+            orderBy: {
+                created_at: 'desc',
+            }
+        });
+
+        const data: ReviewData[] = reviewsFromDb.map(review => ({
+            id: review.id,
+            message: review.message,
+            rating: review.rating,
+            created_at: review.created_at,
+            name_user: review.name_user,
+            url_image_user: review.url_image_user,
+        }));
+        
+        const totalPages = Math.ceil(totalReviews / pageSize);
+        const hasNextPage = page < totalPages;
+
+        return {
+            data: data,
+            metadata: {
+                current_page: page,
+                next_page: hasNextPage,
+                size_page: data.length,
+            }
+        };
+    }
+}
