fd851f384a769fd058479afc7cece3f80a220bb1
YujiYamagishi <yujiyamagishi1@gmail.com>
2025-10-14 16:47:23 -0300

    fix: routes

--- src/infra/api/api.ts
+++ src/infra/api/api.ts
@@ -2,14 +2,14 @@ import { ApiExpress } from "./express/api.express";
 import { GetShoppingCartRoute } from "./express/routes/shoppingCart/getShoppingCartRoute";
 import { UpdateShoppingCartRoute } from "./express/routes/shoppingCart/updateShoppingCartRoute";
 import { CreateShoppingCartRoute } from "./express/routes/shoppingCart/createShoppingCartRoute";
-
+import { ListAllProductsRoute } from "./express/routes/product/list-all.express";
 
 const routes = [
   
   new GetShoppingCartRoute(),
   new UpdateShoppingCartRoute(),
   new CreateShoppingCartRoute(),
-  
+   new ListAllProductsRoute(),
   
 ];
 
